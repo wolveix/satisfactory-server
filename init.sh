91e7983480435b846d35f7e756be45b57781c095
Robert Thomas <31854736+Wolveix@users.noreply.github.com>
2021-04-20 14:48:37 +0100

    Multiple Fixes
    - Dropped save file sync time from 30 minutes to 5 minutes
    - Fixed config files breaking on container restart
    - Fixed wrong save file being selected as the "latest"
    - Removed preloading the game as it's unnecessary

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ set -e
 
 GAMECONFIGDIR="/root/.wine/drive_c/users/root/Local Settings/Application Data/FactoryGame/Saved"
 
-mkdir -p /config /config/gamefiles /config/savefiles /config/savefilebackups "${GAMECONFIGDIR}/SaveGames/common" || true
+mkdir -p /config/gamefiles /config/savefiles /config/savefilebackups "${GAMECONFIGDIR}/Config/WindowsNoEditor" "${GAMECONFIGDIR}/Logs" "${GAMECONFIGDIR}/SaveGames/common" || true
+touch "${GAMECONFIGDIR}/Config/WindowsNoEditor/Engine.ini" "${GAMECONFIGDIR}/Config/WindowsNoEditor/Game.ini" "${GAMECONFIGDIR}/Logs/FactoryGame.log"
 
 if [[ "${STEAMBETA}" == "true" ]]; then
     printf "Experimental flag is set. Experimental will be downloaded instead of Early Access.\\n"
@@ -24,20 +25,13 @@ steamcmd +@sSteamCmdForcePlatformType windows \
 
 cd /config/gamefiles || exit 1
 
-if [[ ! -f "$GAMECONFIGDIR/Config/WindowsNoEditor/Engine.ini" || ! -f "$GAMECONFIGDIR/Config/WindowsNoEditor/Game.ini" ]]; then
-    printf "\\nIt doesn't look like Satisfactory has been started before. Generating config files and inserting cron jobs.\\n"
-    wine start FactoryGame.exe -nosteamclient -nullrhi -nosplash -nosound && sleep 5s
+echo "$(cat /root/Engine.ini)" > "$GAMECONFIGDIR/Config/WindowsNoEditor/Engine.ini"
+echo "$(cat /root/Game.ini)" > "$GAMECONFIGDIR/Config/WindowsNoEditor/Game.ini"
 
-    pkill FactoryGame.exe
-
-    echo "$(cat /root/Engine.ini)" >> "$GAMECONFIGDIR/Config/WindowsNoEditor/Engine.ini"
-    echo "$(cat /root/Game.ini)" > "$GAMECONFIGDIR/Config/WindowsNoEditor/Game.ini" # this won't get created, so we don't append the echo.
-
-    echo "*/30 * * * * cp -rp \"${GAMECONFIGDIR}/SaveGames/common/\"*.sav /config/savefiles/ 2>&1
+echo "*/5 * * * * cp -rp \"${GAMECONFIGDIR}/SaveGames/common/\"*.sav /config/savefiles/ 2>&1
 0 */6 * * * /backup.sh 2>&1" > cronjobs
-    crontab cronjobs
-    service cron start
-fi
+crontab cronjobs
+service cron start
 
 if [[ ! -f "/config/savefiles/savefile.sav" ]]; then
     printf "\\nSave file cannot be found. You need to generate a new world on your client, and then put it into /config/savefiles/savefile.sav\\n"
@@ -45,12 +39,11 @@ if [[ ! -f "/config/savefiles/savefile.sav" ]]; then
 fi
 
 cp -rp /config/savefiles/*.sav "${GAMECONFIGDIR}"/SaveGames/common/
-lastsavepath=$(find "${GAMECONFIGDIR}"/SaveGames/common -name '*.sav' -printf '%p\n' | sort -r | head -n 1)
-lastsavefile=$(basename "$lastsavepath")
+lastsavefile=$(ls -Art "${GAMECONFIGDIR}"/SaveGames/common | tail -n 1)
 if [[ ! "${lastsavefile}" == "savefile.sav" ]]; then
     printf "\\nMoving most recent save (${lastsavefile}) to savefile.sav\\n"
-    mv "${lastsavepath}" "${GAMECONFIGDIR}/SaveGames/common/savefile.sav"
+    mv  "${GAMECONFIGDIR}"/SaveGames/common/"${lastsavefile}" "${GAMECONFIGDIR}/SaveGames/common/savefile.sav"
 fi
 
 wine start FactoryGame.exe -nosteamclient -nullrhi -nosplash -nosound && \
-tail -f "/root/.wine/drive_c/users/root/Local Settings/Application Data/FactoryGame/Saved/Logs/FactoryGame.log"
+tail -f "${GAMECONFIGDIR}/Logs/FactoryGame.log"
