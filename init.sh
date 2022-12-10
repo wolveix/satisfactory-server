b1c0fffbe00fdd2e8c63eb79b60b42e67bffa8b5
Robert Thomas <31854736+wolveix@users.noreply.github.com>
2022-12-10 01:49:54 +0000

    Fix permissions bug (#157)

--- init.sh
+++ init.sh
@@ -43,28 +43,24 @@ mkdir -p \
 
 NUMCHECK='^[0-9]+$'
 
+# check if the user and group IDs have been set
 if ! [[ "$PGID" =~ $NUMCHECK ]] ; then
     printf "Invalid group id given: %s\\n" "$PGID"
     PGID="1000"
+elif [[ "$PGID" -eq 0 ]]; then
+    printf "PGID/group cannot be 0 (root)\\n"
+    exit 1
 fi
 
 if ! [[ "$PUID" =~ $NUMCHECK ]] ; then
     printf "Invalid user id given: %s\\n" "$PUID"
     PUID="1000"
+elif [[ "$PUID" -eq 0 ]]; then
+    printf "PUID/user cannot be 0 (root)\\n"
+    exit 1
 fi
 
-if [[ $(getent group $PGID | cut -d: -f1) ]]; then
-    usermod -a -G "$PGID" steam
-else
-    groupmod -g "$PGID" steam
-fi
-
-if [[ $(getent passwd ${PUID} | cut -d: -f1) ]]; then
-    USER=$(getent passwd $PUID | cut -d: -f1)
-else
-    usermod -u "$PUID" steam
-fi
-
+groupmod -g "$PGID" steam
+usermod -u "$PUID" steam
 chown -R "$PUID":"$PGID" /config /home/steam
-
-exec gosu "$USER" "/home/steam/run.sh" "$@"
+exec gosu steam "/home/steam/run.sh" "$@"
