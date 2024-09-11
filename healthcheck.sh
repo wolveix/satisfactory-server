2992559be0ab5e6c825fdd7792adc1b985162c62
Tan Siewert <tom@siewert.io>
2024-09-11 09:30:10 +0200

    healthcheck: use jq to parse json
    The use of `grep` requires the HTTP server to return the JSON body
    in one line. This could change with an update (either intended or not)
    and would break the healthcheck.
    
    Instead, we should parse the JSON and exit with code 1 if the health
    is not healthy.
    
    Signed-off-by: Tan Siewert <tom@siewert.io>

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,4 +3,4 @@
 curl -k -f -X POST "https://127.0.0.1:${SERVERGAMEPORT}/api/v1" \
 -H "Content-Type: application/json" \
 -d '{"function":"HealthCheck","data":{"clientCustomData":""}}' \
-| grep -q '{"data":{"health":"healthy"'
+| jq -e '.data.health == "healthy"'
