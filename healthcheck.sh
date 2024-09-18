66695d080125101b205bcfe5e92bcf604ee77f21
Valerie Hampe <ashfire908@users.noreply.github.com>
2024-09-18 17:00:14 -0400

    Improved health check (#321)

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -o pipefail
 
-curl -k -f -X POST "https://127.0.0.1:${SERVERGAMEPORT}/api/v1" \
+curl -k -f -s -S -X POST "https://127.0.0.1:${SERVERGAMEPORT}/api/v1" \
 -H "Content-Type: application/json" \
 -d '{"function":"HealthCheck","data":{"clientCustomData":""}}' \
 | jq -e '.data.health == "healthy"'
